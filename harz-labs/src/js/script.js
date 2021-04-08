df89f28b6f680ec61fffc2543397aa094954fdcc
center-1 <center88@mail.ru>
2021-04-08 09:10:53 +0300

    fix bugs, переключение вкладок product block

--- harz-labs/src/js/script.js
+++ harz-labs/src/js/script.js
@@ -1,33 +1,7 @@
 
+document.addEventListener('DOMContentLoaded', () => {
 
-// switch tabs Product block 
-document.addEventListener('DOMContentLoaded', function(e) {
-  let productLink = document.querySelectorAll('.product__link');
-
-  productLink = Array.prototype.slice.call(productLink, 0);
-
-  productLink.forEach(function(el, i, ar) {
-    el.addEventListener('click', function(event){
-      e.preventDefault();
-
-      let tabLink = document.querySelector(el.getAttribute('href'));
-
-      document.querySelector('.product__link .product__link--active')
-        .classList.remove('product__link--active');
-
-      document.querySelector('.product .product--active')
-        .classList.remove('product--active');
-
-      el.classList.add('product__link--active');
-      tabLink.classList.add('product--active');
-    })
-  })
-})
-
-
-
-
-// counter price 
+// counter price / счётчик цены 
 let buttonCountPlus = document.getElementById("buttonCountPlus");
 let buttonCountMinus = document.getElementById("buttonCountMinus");
 let count = document.getElementById("buttonCountNumber");
@@ -50,4 +24,27 @@ buttonCountMinus.onclick = function() {
     count2.value = number * price;
   }
 };
+
+
+// switch tabs Product block / переключение вкладок
+let productLink = document.querySelectorAll('.product__link');
+
+productLink = Array.prototype.slice.call(productLink, 0);
+
+productLink.forEach(function(el, i, ar) {
+  el.addEventListener('click', function(event){
+    let tabLink = document.querySelector(el.getAttribute('href'));
+
+    document.querySelector('.product__link--active')
+      .classList.remove('product__link--active');
+
+    document.querySelector('.product .product--active')
+      .classList.remove('product--active');
+
+    el.classList.add('product__link--active');
+    tabLink.classList.add('product--active');
+  });
+});
+
+});  // DOMContentLoaded / ожидание загрузки DOM-дерева
 
